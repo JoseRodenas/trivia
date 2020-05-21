a930250f6e9db7477c41800c3e3e6e6bdc5a0b8f
Jose <7476903@alu.murciaeduca.es>
2020-05-21 12:42:43 +0200

    primer modelo preguntas

---
+++ database/migrations/2020_05_21_101640_create_preguntas_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePreguntasTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('preguntas', function (Blueprint $table) {
+            $table->id();
+            $table->string('pregunta',200);
+            $table->string('respuesta1',50);
+            $table->string('respuesta2',50);
+            $table->string('respuesta3',50);
+            $table->string('respuesta4',50);
+            $table->string('categoria',20);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('preguntas');
+    }
+}
