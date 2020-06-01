75fd18ae7784b77c7628a05fa971a43519d0937a
Jose <7476903@alu.murciaeduca.es>
2020-06-01 20:36:46 +0200

    Prueba heroku

--- database/seeds/UsersTableSeeder.php
+++ database/seeds/UsersTableSeeder.php
@@ -21,7 +21,7 @@ class UsersTableSeeder extends Seeder
                 'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
                 'remember_token' => Str::random(10),
             ]);
-            $users = factory(\App\User::class, 9)->create();
+            /* $users = factory(\App\User::class, 9)->create(); */
         }
     }
 }
